73a2c761e028af0dff09139e9b98f1160ff41836
Christopher Keiningham <130263834+chriskein1@users.noreply.github.com>
2024-06-18 16:36:46 -0500

    Goal text

---
+++ Assets/Scripts/GameLogic/Goal.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+using QubitType;
+public class Goal : MonoBehaviour
+{
+    [SerializeField] GamePuzzleController gamePuzzleController;
+    private TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the win state
+        Qubit winState = gamePuzzleController.GetWinState();
+
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        UpdateText(winState);
+    }
+    private void UpdateText(Qubit winState)
+    {
+        string sign = winState.PositiveState ? "" : "-";
+        string qubitStr = "";
+
+        if (winState.HApplied)
+        {
+            if (winState.PositiveState)
+            {
+                qubitStr = winState.ImaginaryState ? "i/√2(|0> + |1>)" : "1/√2(|0> + |1>)";
+            }
+            else
+            {
+                qubitStr = winState.ImaginaryState ? "i/√2(|0> - |1>)" : "1/√2(|0> - |1>)";
+            }
+        }
+        else if (winState.ImaginaryState)
+        {
+            qubitStr = $"{sign}i|{winState.state}>";
+        }
+        else
+        {
+            qubitStr = $"{sign}|{winState.state}>";
+        }
+
+        text.text += qubitStr;
+    }
+}
