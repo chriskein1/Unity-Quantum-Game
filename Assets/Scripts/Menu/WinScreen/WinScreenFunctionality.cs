ff2c5a9b39fa5ccd03b44e53f4abccaa98eabd92
TannerLew <ltanner1121@gmail.com>
2024-06-20 11:35:23 -0500

    Win screen animation

--- Assets/Scripts/Menu/WinScreen/WinScreenFunctionality.cs
+++ Assets/Scripts/Menu/WinScreen/WinScreenFunctionality.cs
@@ -1,18 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WinScreenFunctionality : MonoBehaviour
 {
-    private Animator animator;
-
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
-
-    void OnEnable()
-    {
-        animator.SetTrigger("start");
-    }
+    
 }
